f15ade32811df26b6ea64c79d33483a377fa474a
Zhan925 <73823803+Zhan925@users.noreply.github.com>
2020-11-02 19:13:41 +0800

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,11 @@
+public class Time{
+    int hours;
+    int minutes;
+    Time(int hours, int minutes){
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+    public int TimeinMinutes(){
+        return this.hours*60 + this.minutes;
+    }
+}
